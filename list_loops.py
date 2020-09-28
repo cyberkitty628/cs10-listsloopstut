8b88af81e8d982f408fbafb7e6fdfe999699bbac
Kristina <cyberkitty628@gmail.com>
2020-09-27 20:06:15 -0700

    Completed tutorial

--- list_loops.py
+++ list_loops.py
@@ -2,5 +2,19 @@ songs = ["ROCKSTAR", "Do It", "For The Night"]
 
 print(songs[0])
 print(songs[2])
-
 print(songs[1:3])
+songs[2] = "Savage"
+print(songs)
+
+songs.append("Cherish the Day")
+songs.extend(["Make It Right"])
+songs.insert(5, "Juicy")
+songs.remove("Do It")
+
+animals = ["cat", "quokka", "bunny"]
+animals.append("snow leopard")
+print(animals[2])
+del animals[0]
+
+for animal in animals:
+    print(animal)
